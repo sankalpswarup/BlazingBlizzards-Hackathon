fa38f78cc7166df3178ec33ab8406fc359565960
tejasverma21 <79651962+tejasverma21@users.noreply.github.com>
2021-02-27 11:48:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from flask import Flask, redirect, url_for, render_template, request
 
+from tmdbv3api import Movie
+from tmdbv3api import Discover
+from tmdbv3api import TMDb
+tmdb = TMDb()
+tmdb.api_key = '0c8cda0ec2f25eaa3eaf18991dde2e6c'
+
+
 app=Flask(__name__)
 
 
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.route('/')
 
-@app.after_request
-def add_header(r):
+def index():
+    movie=Movie()
+    discover = Discover()
+    movie1 = discover.discover_movies({'sort_by' : 'popularity.desc'})
+    temp=''
+    for m in movie1:
+        temp+=m.title
+        temp+='<br>'
+    return render_template('index.html', mov=temp)
 
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["POST"])
 def home():
-    if request.method == "POST":
-	    user = request.form["movie_field"]    
-                                                    #movie_field in the above line has the data entered in the search field on the webpage.   
-	    return redirect(url_for("user", usr=user))
-    else:
-	    return render_template("index.html")
-
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
-
-if __name__=="__main__":
-    app.run()
+    
+    movie = Movie()
+    search = movie.search(request.form["movie_field"])
+    s=''
+    for res in search:
+        s+='<p style="text-align:center;font-size:30px;">'
+        s+=(res.title)
+        s+='</p>'
+        #movie_field in the above line has the data entered in the search field on the webpage.
+    
+    return render_template('pass.html', movies=s)
+if __name__=='__main__':
+    app.run(debug=True)
